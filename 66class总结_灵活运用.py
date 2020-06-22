356d5d82f63d1e2ee26bda85272460da60001a21
ycallenchina <ycallen@126.com>
2020-06-22 10:02:42 +0800

    Create 66class总结_灵活运用.py

---
+++ 66class总结_灵活运用.py
@@ -0,0 +1,29 @@
+
+
+class list_update(list):
+
+
+	def print_lines(self): 
+		for i in self:
+			print(i)
+
+
+x=list_update([1,2,3])
+x.print_lines()
+
+
+
+class list_update2(list):
+
+
+	def __repr__(self): 
+		for i in self:
+			print(i)
+		return ''
+
+y=list_update2([1,2,3])
+print(y)
+
+class shuxue():
+	shuxue.name='高数'
+	shuxue.power=100
