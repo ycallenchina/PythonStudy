be8888cdf883713c61b09c9ee263006f396b27c4
ycallenchina <ycallen@126.com>
2020-07-29 23:50:36 +0800

    Update 22搜索系统_窗体版.py

--- 22搜索系统_窗体版.py
+++ 22搜索系统_窗体版.py
@@ -23,7 +23,7 @@ def getnew():
     # s=re.findall(r"\d+\.?\d*",str(mulu_py))
 
     # 得到后缀py的目录mulu_py
-    mulu=os.listdir('e:\pyNote')
+    mulu=os.listdir('e:\PythonStudy_Git')
     mulu_py=[]
     for i in mulu:
         if '.py' in i:
@@ -37,10 +37,10 @@ def getnew():
         k=k+1
         # 先写入目录名,并打开此目录
         w.write(str(k)+'___'+i + '\n\n')
-        f = open('e:\pyNote/' + i, 'r', encoding='utf-8')
+        f = open('e:\PythonStudy_Git/' + i, 'r', encoding='utf-8')
         #提取lines内容,带有#的注释内容,并写入w
         for i in f.readlines():
-            if '#' in i:
+            if '' in i:
                 k=k+1
                 w.write(str(k)+'        '+i+'\n')
     print('更新成功')
