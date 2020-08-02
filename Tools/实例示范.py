dfd2df6d973bbb9113cf5faae97b0838c7e73c05
ycallenchina <ycallen@126.com>
2020-08-02 11:13:06 +0800

    Update 实例示范.py

--- Tools/实例示范.py
+++ Tools/实例示范.py
@@ -1,7 +1,7 @@
 
 def 示范1():
     '''搜头文件里所有.py的代码'''
-    from Tools import find_db
+    import find_db
     头文件='E:/pyNote'
     find_db.内容搜索窗口(find_db.返回列表中文件里所有内容(find_db.找遍所有文件里某类型文件的路径(头文件),头文件))
 
