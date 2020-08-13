c279a779d11112b223b8c7d7949c21318991b13f
ycallenchina <ycallen@126.com>
2020-08-13 17:54:09 +0800

    Update demo剩余表算法.py

--- 进销存系统/demo剩余表算法.py
+++ 进销存系统/demo剩余表算法.py
@@ -1,81 +1,55 @@
 import pandas as pd
 import numpy as np
 import sys
-import  pymysql
-import  pymysql.cursors
+sys.path.append("..")#为了import引用上一级包
+from Tools.sql_db import *
 
-#获取一个数据库连接，注意如果是UTF-8类型的，需要制定数据库
-#port 必须是数字不能为字符串
-#db是 数据库名
-connection=pymysql.connect(host='localhost',
-                           user='root',
-                           password='123456',
-                           db='test_data',
-                           port=3306,
-                           charset='utf8')
-cursor = connection.cursor()
-
-def 内嵌销量表(CL_name0,CL_need):#含有递归算法
+def 内嵌销量表(CL_name,CL_need):#含有递归算法
 	
-	CL_sheet=CL_name0
-	cursor.execute(f"select 材料 from {CL_name0}")
-	CL_list=cursor.fetchall()
+	CL_sheet=CL_name
+	CL_list=sql表的材料list(CL_sheet)
+
 	for CL_name in CL_list:
-		if CL_name[0] in sheetname_All_GlobalX:
-			CL_name0=CL_name[0]
-			cursor.execute(f"select 用量 from {CL_sheet} where 材料='{CL_name[0]}'")
-			sql_fig=cursor.fetchall()
-			CL_need=float(sql_fig[0][0])*CL_sales_GlobalX#第二次材料表实际的总用量
-			内嵌销量表(CL_name0,CL_need)
+
+		CL_need_percent=sql材料的用量(CL_sheet,CL_name)/sql表的总用量(CL_sheet)
+
+		if CL_name in sheetname_All_GlobalX:
+			CL_need=CL_need*CL_need_percent#第二次材料表实际的用量
+			sheetname_All_GlobalX.remove(CL_name)#递归反死循环机制,每进入下一级,删除本级元素.防无线递归.
+			内嵌销量表(CL_name,CL_need)
 		else:
-			cursor.execute(f"select 用量 from {CL_sheet} where 材料='{CL_name[0]}'")
-			sql_fig1=cursor.fetchall()#配方数据
-
-			cursor.execute(f"select 用量 from 材料库 where 材料='{CL_name[0]}'")
-			sql_fig2=cursor.fetchall()#库数据
-			
 			#用获取的材料名对应的数值相减;算出本次库存剩余数据,并updata到材料库里
-			updata_db=float(sql_fig2[0][0])//float(sql_fig1[0][0])
-			CL_sheet_MinCount[CL_name[0]]=updata_db
-			# CL_sheet_MinCount.append(updata_db)
-			# CL_sheet_MinCount.append(CL_name[0])
-			# cursor.execute(f"UPDATE 材料库 SET 用量 = '{updata_db}' WHERE 材料='{CL_name[0]}'")
-			# print(f"UPDATE 材料库 SET 用量 = '{updata_db}' WHERE 材料='{CL_name[0]}'")
+			#用第二次材料表实际的总用量 乘以 这次材料所在总用量中的占比,算出实际用量
+			sql_fig2=sql材料的用量('材料库',CL_name)
+			updata_db=sql_fig2//(CL_need*CL_need_percent)
+			updata_db=round(updata_db,2)#保留2位小数
+			CL_sheet_MinCount[CL_name]=updata_db
 
-df=pd.read_excel('E:/PythonStudy_Git/调用资料/配方表.xlsx',sheet_name = None)#读取excel表格
+df=pd.read_excel('E:/PythonStudy_Git/调用资料/配方表.xlsx',sheet_name = None)#读取excel表格,所有sheet表
 sheetname_All_GlobalX=[i for i in df]#获取已有配方表名
 
-df=pd.read_excel('E:/PythonStudy_Git/调用资料/销量表.xlsx',sheet_name = 0)#读取excel表格
+df=pd.read_excel('E:/PythonStudy_Git/调用资料/销量表.xlsx',sheet_name = 0)#读取excel表格,第一张sheet表
 for index,row in df.iterrows():#获取销量表的菜单名,及销量
-	CL_sheet=row[0]
-	CL_sales_GlobalX=float(row[1])
-	cursor.execute(f"select 材料 from {CL_sheet}")
-	CL_list=cursor.fetchall()
+	CL_sheet,CL_sales_GlobalX=row
+	CL_list=sql表的材料list(CL_sheet)
 	CL_sheet_MinCount={}
 
 	#执行sql获取表的材料名
 	for CL_name in CL_list:#遍历sql表咖喱酱里的材料名
-		if CL_name[0] in sheetname_All_GlobalX:
-			CL_name0=CL_name[0]
-			cursor.execute(f"select 用量 from {CL_sheet} where 材料='{CL_name[0]}'")
-			sql_fig=cursor.fetchall()
-			CL_need=float(sql_fig[0][0])*CL_sales_GlobalX#材料用量=单位用量*销量
-			内嵌销量表(CL_name0,CL_need)
+		if CL_name in sheetname_All_GlobalX:
+			CL_need=sql材料的用量(CL_sheet,CL_name)
+			sheetname_All_GlobalX.remove(CL_name)#递归反死循环机制,每进入下一级,删除本级元素.防无线递归.
+			内嵌销量表(CL_name,CL_need)
 		else:
-			cursor.execute(f"select 用量 from {CL_sheet} where 材料='{CL_name[0]}'")
-			sql_fig1=cursor.fetchall()#配方数据
-
-			cursor.execute(f"select 用量 from 材料库 where 材料='{CL_name[0]}'")
-			sql_fig2=cursor.fetchall()#库数据
+			sql_fig1=sql材料的用量(CL_sheet,CL_name)#配方数据
+			sql_fig2=sql材料的用量('材料库',CL_name)#库数据
 			
 			#用获取的材料名对应的数值相减;算出本次库存剩余数据,并updata到材料库里
-			updata_db=float(sql_fig2[0][0])//float(sql_fig1[0][0])
-			CL_sheet_MinCount[CL_name[0]]=updata_db
-			# CL_sheet_MinCount.append(updata_db)
-			# CL_sheet_MinCount.append(CL_name[0])
-			# cursor.execute(f"UPDATE 材料库 SET 用量 = '{updata_db}' WHERE 材料='{CL_name[0]}'")
-			# print(f"UPDATE 材料库 SET 用量 = '{updata_db}' WHERE 材料='{CL_name[0]}'")
-	print(f'{CL_sheet}    的最多可出:',min(CL_sheet_MinCount.values()))
+			updata_db=sql_fig2//sql_fig1
+			updata_db=round(updata_db,2)#保留2位小数
+			CL_sheet_MinCount[CL_name]=updata_db
+
+	print(f'{CL_sheet}    的最多可出:',min(CL_sheet_MinCount.values()))#
 	print({i:CL_sheet_MinCount[i] for i in CL_sheet_MinCount if CL_sheet_MinCount[i]==min(CL_sheet_MinCount.values())})
-	# print(CL_sheet_MinCount)
+	print(CL_sheet_MinCount)
 connection.close()
