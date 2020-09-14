bf258c9486d769eafbf8ec64a3d0355dda90cacc
ycallenchina <ycallen@126.com>
2020-09-14 23:35:35 +0800

    Update 73装饰器.py

--- 73装饰器.py
+++ 73装饰器.py
@@ -10,6 +10,7 @@ def senior_q(f):#高级的senior
 
 def for_girth(r):#求周长girth
 	return 3.14*r*2
+
 for_girth=senior_q(for_girth)
 
 print(for_girth(3))
@@ -17,16 +18,17 @@ print(for_girth(3))
 
 
 #装饰器@wraps的用法
+print('#装饰器@wraps的用法----------------------')
 from functools import wraps 
 
 def q(f):
 	@wraps(f)
 	def new(r):
-		print('@wraps已装饰')
-		return f(r)
+		new33=str(f(r))+':我已经被装饰'
+		return new33
 	return new
 
-@q
+@q#作用等于p=q(p),所以p已经被装饰
 def p(r):
 	return 3.14*r*2
 
