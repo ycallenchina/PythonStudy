3c168fbc05630885396a1e537d1ca7def23ac489
ycallenchina <ycallen@126.com>
2020-11-09 18:18:21 +0800

    Update 59class类的构造方法.py

--- 59class类的构造方法.py
+++ 59class类的构造方法.py
@@ -1,10 +1,15 @@
+
+#class 构造方法,初始化方法
+#__init__的使用
+
 #实例:小马赛跑
 import random
 import time
 class Game:
     #属性,可用默认值,例如:s=2
-    def __init__(self,n,s=2):
-        self.name=n
+    def __init__(self,n,s=2):#构造方法,对象创建时,会自动调用运行一次.
+        staff='我是内部工作人员'#局部变量
+        self.name=n#对象的属性 name
         self.speed=s
         self.loc=0
         
@@ -15,6 +20,7 @@ class Game:
 ma_1=Game('红马',5)
 ma_2=Game('黄马',3)
 ma_3=Game('青马',6)
+
 while 1:
     ma_1.run()
     ma_2.run()
