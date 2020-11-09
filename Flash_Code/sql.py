743807d3e6bb6f50521b6e151a533a1a283f7b9f
ycallenchina <ycallen@126.com>
2020-11-09 18:18:38 +0800

    Update sql.py

--- Flash_Code/sql.py
+++ Flash_Code/sql.py
@@ -1,6 +1,6 @@
 # import pandas as pd
 # import numpy as np
-import sys
-sys.path.append("..")#为了import引用上一级包
-from Tools.sql_db import *
-from TK学习.监控变量 import *
+# import sys
+# sys.path.append("..")#为了import引用上一级包
+# from Tools.sql_db import *
+# from TK学习.监控变量 import *
