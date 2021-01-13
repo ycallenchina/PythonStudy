e84ed993d3855ff439480c236c7a68cdfe2d2ed9
ycallenchina <63100652+ycallenchina@users.noreply.github.com>
2021-01-13 13:57:52 +0800

    Update 实例_excel每日报表合并.py

--- Pandas学习/实例_excel每日报表合并.py
+++ Pandas学习/实例_excel每日报表合并.py
@@ -35,8 +35,9 @@ cai_index=(('1','2020-09-01'),
 for i in cai_index:
 	#逐个打开表,并增加时间列
 	df=pd.read_excel(f'E:/PythonStudy_Git/调用资料/file/菜品报表 ({i[0]}).xlsx',sheet_name = 0)
-	#增加时间
+	#增加时间列
 	df['时间']=i[1]
+	#合并两表,方式是outer
 	df_init=pd.merge(df_init,df,how="outer")
 
 # print(cai_index)
