0f8a47ba3a9a166fd0e396519b8da943ba172d82
ycallenchina <63100652+ycallenchina@users.noreply.github.com>
2021-11-19 17:23:18 +0800

    Update 实时股价监控.py

--- 其他学习_少量/量化交易学习/实时股价监控.py
+++ 其他学习_少量/量化交易学习/实时股价监控.py
@@ -1,6 +1,7 @@
 
 import easyquotation
 import time
+
 def time_out(late=100):#随机时间函数
 	import time
 	import random
@@ -9,16 +10,22 @@ def time_out(late=100):#随机时间函数
 	# print('延时',rand,'秒...')
 	return rand
 
-for i in range(10):
-	time_start = time.time()
-	quotation = easyquotation.use('tencent') # 新浪 ['sina'] 腾讯 ['tencent', 'qq']
-	tmp_dict = quotation.market_snapshot(prefix=True) # prefix 参数指定返回的行情字典中的股票代码 key 是否带 sz/sh 前缀
-	time_end = time.time()
-	time_use = time_end-time_start
-	print(
-	tmp_dict['sz000001']['name'],
-	tmp_dict['sz000001']['now'],
-	'Now:',time.strftime("%m-%d %H:%M:%S", time.localtime()) ,
-	'Pass:',round(time_use,2),'秒')#查看实时股价
+def go():
 
-	print('------------------------------------------------')
+	for i in range(10):
+		time_start = time.time()
+		quotation = easyquotation.use('tencent') # 新浪 ['sina'] 腾讯 ['tencent', 'qq']
+		tmp_dict = quotation.market_snapshot(prefix=True) # prefix 参数指定返回的行情字典中的股票代码 key 是否带 sz/sh 前缀
+		time_end = time.time()
+		time_use = time_end-time_start
+		
+		name=tmp_dict['sz000001']['name']
+		code=str(tmp_dict['sz000001']['now'])
+		now='Now:'+str(time.strftime("%m-%d %H:%M:%S", time.localtime()))
+
+		print(name,code,now,'Pass:',round(time_use,2),'秒')#查看实时股价
+		print('------------------------------------------------')
+
+
+
+go()
