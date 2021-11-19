f4b3ec1f0afd2a02a4def0c2c3138b8537b3c3a0
ycallenchina <63100652+ycallenchina@users.noreply.github.com>
2021-11-19 17:23:34 +0800

    Update pandas实验室.py

--- Pandas学习/pandas实验室.py
+++ Pandas学习/pandas实验室.py
@@ -102,8 +102,11 @@ def 插入索引列(df):
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', None)#显示所有df列
-    路径='C:/Users/YcAllenEffy/Desktop/新兴股价表.csv'
-    df=读取csv(路径)
-    print(df)
+    # print(df1)
+    df=df1[['height','weight','age']]
+    df.to_csv('C:/Users/YcAllenEffy/Desktop/样本.csv')
+    # 路径='C:/Users/YcAllenEffy/Desktop/新兴股价表.csv'
+    # df=读取csv(路径)
+    # print(df)
 
 
