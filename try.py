aec7134f99ae7989cfaeedb2db48aa3be9118ee5
ycallen <ycallen@126.com>
2021-12-05 18:20:28 +0800

    del to add try实验室Folder

--- try.py
+++
@@ -1,4 +0,0 @@
-
-import sys
-sys.path.append('D:/PythonStudy_Git/股票量化实践')
-import df股票数据处理 as 处理
--- try2.py
+++
@@ -1,9 +0,0 @@
-
-
-import hashlib
-
-md5=hashlib.sha256()
-
-md5.update('effy.2'.encode('utf-8'))
-print(md5.hexdigest())
-
