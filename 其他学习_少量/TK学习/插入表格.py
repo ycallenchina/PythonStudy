12db68265004e30fd00c1042f33ac1960922b238
ycallen <ycallen@126.com>
2021-12-06 21:53:26 +0800

    Update 插入表格.py

--- 其他学习_少量/TK学习/插入表格.py
+++ 其他学习_少量/TK学习/插入表格.py
@@ -1,6 +1,3 @@
-
-
-
 from tkinter import *
 from tkinter import ttk
 
@@ -11,7 +8,8 @@ root.title('变量监视')#设置标题
 dic=['allen','yc','qqqq']
 dic2=[1,2,3]
 
-lable = Label(root, text="变量监控", font=("Fixedsys", 12), width=8, height=1)
+# lable = Label(root, text="变量监控", font=("Fixedsys", 12), width=8, height=1)
+# lable.place(x=0,y=0)
 
 #表格
 tree = ttk.Treeview(root,columns=['1','2','3'],show='headings',height=30)#设置表
@@ -21,14 +19,9 @@ tree.column('1',width=50)
 tree.column('2',width=50,anchor='center')
 tree.column('3',width=50,anchor='center')
 #设置表名
-# tree.heading('1',text='变量')
-
+tree.heading('3',text='i变量')
 
 #插入数值
-tree.heading('3',text='i变量')
-# for i in range(20):
-
 tree.insert('','end',values=dic)
 tree.insert('','end',values=dic2)
-lable.place(x=0,y=0)
 root.mainloop()#显示窗体
